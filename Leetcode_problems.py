67cf75516ff8e81402ff4901de554d904faf68da
bhuvana <bhuvanadongade@gmail.com>
2026-07-22 10:00:19 +0530

    code

--- Leetcode_problems.py
+++ Leetcode_problems.py
@@ -245,4 +245,304 @@ class Solution:
             max_reach = max(max_reach, i + nums[i])
 
         return True
+
+
+Problem no-137
+137. Single Number II
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: nums = [0,1,0,1,0,1,99]
+Output: 99
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-231 <= nums[i] <= 231 - 1
+Each element in nums appears exactly three times except for one element which appears once.
+
+### answer
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ones = 0
+        twos = 0
+
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+
+        return ones
+
+Problem no: 605
+605. Can Place Flowers
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+ 
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length
+
+## answer
+class Solution:
+    def canPlaceFlowers(self, flowerbed, n):
+        for i in range(len(flowerbed)):
+            if (flowerbed[i] == 0 and
+                (i == 0 or flowerbed[i - 1] == 0) and
+                (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)):
+                
+                flowerbed[i] = 1
+                n -= 1
+
+                if n == 0:
+                    return True
+
+        return n <= 0
+
+Problem no: 1207
+1207. Unique Number of Occurrences
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000
+
+## answer
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        count = {}
+
+        for num in arr:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+
+        return len(count.values()) == len(set(count.values()))
+Problem no-724
+724. Find Pivot Index
+Given an array of integers nums, calculate the pivot index of this array.
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+Return the leftmost pivot index. If no such index exists, return -1.
+
+Example 1:
+
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+Example 2:
+
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+Example 3:
+
+Input: nums = [2,1,-1]
+Output: 0
+Explanation:
+The pivot index is 0.
+Left sum = 0 (no elements to the left of index 0)
+Right sum = nums[1] + nums[2] = 1 + -1 = 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-1000 <= nums[i] <= 1000
+
+## answer
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total_sum = sum(nums)
+        left_sum = 0
+
+        for i in range(len(nums)):
+            right_sum = total_sum - left_sum - nums[i]
+
+            if left_sum == right_sum:
+                return i
+
+            left_sum += nums[i]
+
+        return -1
+Problem no-643
+643. Maximum Average Subarray I
+Easy
+Topics
+premium lock icon
+Companies
+You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+Example 2:
+
+Input: nums = [5], k = 1
+Output: 5.00000
+ 
+
+Constraints:
+
+n == nums.length
+1 <= k <= n <= 105
+-104 <= nums[i] <= 104
+
+## answer
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        window_sum = sum(nums[:k])
+        max_sum = window_sum
+
+        for i in range(k, len(nums)):
+            window_sum = window_sum + nums[i] - nums[i - k]
+            max_sum = max(max_sum, window_sum)
+
+        return max_sum / k   
+Problem no-392
+392. Is Subsequence
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+
+##answer
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+         i = 0
+         j = 0
+
+         while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+         return i == len(s)
+Problem no-283
+283. Move Zeroes
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+
+## answer
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[k], nums[i] = nums[i], nums[k]
+                k += 1
 '''
